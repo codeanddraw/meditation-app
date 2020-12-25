afa661ea321226d9cd3b1bab00fed1bd0be2ac30
Nisha <nisha@Nishas-MacBook-Pro.local>
2020-12-25 17:31:26 -0500

    Add the video and audio play/pause

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const app = () => {
 
     const sounds = document.querySelectorAll('.sound-picker button')
     const timeDisplay = document.querySelector('.time-display')
+    const timeSelect = document.querySelectorAll('.time-select button') 
 
     const outlineLength = outline.getTotalLength();
 
     let fakeDuration = 600;
-    outline.style.strokeDashArray = outlineLength;
-    outline.style.strokeDashOffset = 500;
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
 
     play.addEventListener('click',() => {
         checkPlaying(song)
@@ -20,19 +21,48 @@ const app = () => {
     const checkPlaying = song => {
         if(song.paused) {
             song.play()
+            video.play()
             play.src = './svg/pause.svg'
         } else {
             song.pause()
+            video.pause()
             play.src = './svg/play.svg'
         }
 
     }
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function(){
+            song.src = this.getAttribute('data-sound')
+            video.src = this.getAttribute('data-video')
+            checkPlaying(song)
+        })
+    })
+  
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() {
+            fakeDuration = this.getAttribute("data-time")
 
+            timeDisplay.textContent = `${Math.floor(fakeDuration/60)}:${Math.floor(fakeDuration%60)}`
+        })
+    })
     song.ontimeupdate = () => {
         let currentTime = song.currentTime
         let elapsed = fakeDuration - currentTime
+        let seconds = Math.floor(elapsed % 60)
+        let minutes = Math.floor(elapsed / 60)
+    
+    
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength
+        outline.style.strokeDashoffset = progress
+
+        timeDisplay.textContent = `${minutes}:${seconds}`
+
+        if(currentTime >= fakeDuration) {
+            song.pause()
+            song.currentTime = 0;
+            play.src = './svg/play.svg'
+        }
     }
-   
 }
 
 app()
